807a0c494ae0d1e27c797ddc913664b37d778942
titouancv <titouan.carion.vignaud@gmail.com>
2026-08-17 16:52:25 +0200

    wip

--- mougins/src/components/ui/BackButton.tsx
+++ mougins/src/components/ui/BackButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ROUTES } from "@/constants";
 import { useRouter } from "@/i18n/navigation";
 import { Button } from "./Button";
 
@@ -10,7 +11,15 @@ interface BackButtonProps {
 
 export function BackButton({ label, onBack }: BackButtonProps) {
   const router = useRouter();
-  const handleBack = onBack ?? (() => router.back());
+  const handleBack =
+    onBack ??
+    (() => {
+      if (window.history.length > 1) {
+        router.back();
+      } else {
+        router.push(ROUTES.HOME);
+      }
+    });
 
   return (
     <Button variant="ghost" type="button" size="none" onClick={handleBack}>
